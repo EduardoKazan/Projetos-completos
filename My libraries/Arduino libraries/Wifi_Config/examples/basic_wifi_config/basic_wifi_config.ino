af700d341a8d5eaafcdb6c20e2e3ef96f8e5a8c2
Eduardo <eduardo_kazan@hotmail.com>
2025-05-19 18:23:38 -0300

    library created

---
+++ My libraries/Arduino libraries/Wifi_Config/examples/basic_wifi_config/basic_wifi_config.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+#include "Wifi_Config.h"
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("Conectado ao WiFi!");
+}
+
+void loop() {
+  // Código do loop
+}
